f92ea1f1b3e298d831a707d326e4c13ae67034db
Nikita Philippov <philipesko@yandex.ru>
2019-09-07 19:09:01 +0300

    Update megabot to version 1.0.0.1
    Signed-off-by: Nikita Philippov <philipesko@yandex.ru>

--- megabot/bot.py
+++ megabot/bot.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 #Proxy settings
 PROXY = {'proxy_url': 'socks5://t2.learn.python.ru:1080', 'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
@@ -9,8 +9,13 @@ def greet_user(bot, update):
     text = 'Call /start'
     print(text)
     update.message.reply_text(text)
-    logging.info('Send email to the customer')
+    logging.info('Send answer to the client')
 
+def talk_to_me(bot, update):
+    user_text = update.message.text
+    print(user_text)
+    update.message.reply_text(user_text)
+    logging.info('Receive message text from client:  '+ user_text)
 #bot
 def main():
     mybot = Updater("812960937:AAEXGUPi7Au90FxLNA0_Y947goTAzbXNS3A", request_kwargs=PROXY)
@@ -18,6 +23,7 @@ def main():
     logging.info('Бот запускается')
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     mybot.start_polling()
     mybot.idle()
