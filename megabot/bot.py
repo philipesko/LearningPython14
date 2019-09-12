756469fb8b9d1e82a48990f5f2c654a55efe23ee
Nikita Philippov <philipesko@yandex.ru>
2019-09-12 20:20:13 +0300

    Home Mission 12/09/2019, 13/09/2019 (While, Exeptions\mission1\mission2) Update telebot.
    Signed-off-by: Nikita Philippov <philipesko@yandex.ru>

--- megabot/bot.py
+++ megabot/bot.py
@@ -1,8 +1,25 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import logging,passwd
+import logging, passwd, ephem
+from datetime import datetime
 #Proxy settings
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO, filename='bot.log')
+
+
+def greet_planet(bot, update):
+    update.message.reply_text('Please write name for planet!')
+    update.planets(bot, update)
+
+def planets (bot, update):
+    arrt_planet_name = update.message.text
+    print(arrt_planet_name)
+    planet = getattr(ephem, arrt_planet_name)(datetime.now())
+    cons = ephem.constellation(planet)
+    print(cons)
+    update.message.reply_text(cons)
+
+
+
 
 def greet_user(bot, update):
     text = 'Call /start'
@@ -23,6 +40,9 @@ def main():
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(CommandHandler('planet', greet_planet))
+    dp.add_handler(MessageHandler(Filters.text, planets))
+
 
     mybot.start_polling()
     mybot.idle()
