61f754492afe9a1a5fcd17bf48b735dc98ad2f5f
Nikita Philippov <philipesko@yandex.ru>
2019-09-12 09:55:39 +0300

    Home Mission 12/09/2019, 13/09/2019 (While, Exeptions\mission1\mission2)
    Signed-off-by: Nikita Philippov <philipesko@yandex.ru>

--- lesson1/sale.py
+++ lesson1/sale.py
@@ -8,16 +8,18 @@ stock = [
 
 
 def discounted(price, discount, max_discount=20, name=''):
-    price = abs(float(price))
-    discount = abs(float(discount))
-    max_discount = abs(float(max_discount))
-    if max_discount > 99:
-        raise ValueError('Слишком большая максимальная скидка')
-    if discount >= max_discount or "iphone" in name.lower() or not name:
-        return price
-    else:
-        return price - (price * discount / 100)
-
+    try:
+        price = abs(float(price))
+        discount = abs(float(discount))
+        max_discount = abs(float(max_discount))
+        if max_discount > 99:
+            raise ValueError('Слишком большая максимальная скидка')
+        if discount >= max_discount or "iphone" in name.lower() or not name:
+            return price
+        else:
+            return price - (price * discount / 100)
+    except(TypeError, ValueError):
+        print("No corrected arguments. Please try again! ")
 
 dis_iphone = discounted(phone1['price'], phone1['discount'], name=phone1['name'])
 print(dis_iphone)
